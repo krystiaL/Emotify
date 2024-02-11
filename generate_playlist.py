21099ce80cbba0088fb990a34f682fc6846f1680
Atsuto Tatsumi <atsutout@gmail.com>
2024-02-11 17:48:22 +0900

    Added params.py file, .envrc file

--- generate_playlist.py
+++ generate_playlist.py
@@ -1,28 +1,23 @@
-import pandas as pd
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-import os
-
-########VARIABLES######
-SPOTIFY_ID = os.environ.get("SPOTIFY_ID")
-SPOTIFY_SECRET = os.environ.get("SPOTIFY_SECRET")
-SPOTIFY_USERNAME = os.environ.get("SPOTIFY_USERNAME")
-REDIRECT_URI = os.environ.get("REDIRECT_URI")
+from Music_Selector_Project.params import *
 
 def generate_playlist(df):
     '''This function will access Spotify API and add playlist to your account.
-    The playlist will be chosen randomly from the df.'''
+    The songs will be chosen randomly from the df.'''
+
     sp = spotipy.Spotify(
         auth_manager=SpotifyOAuth(
             scope="playlist-modify-public",
             redirect_uri=REDIRECT_URI,
-            client_id=SPOTIFY_ID,
+            client_id=SPOTIFY_CLIENT_ID,
             client_secret=SPOTIFY_SECRET,
             show_dialog=True,
             cache_path="token.txt",
             username=SPOTIFY_USERNAME,
         )
     )
+
     user_id = sp.current_user()["id"]
 
     new_playlist = sp.user_playlist_create(user=user_id, name="Test_Playlist", public=True,
---
+++ params.py
@@ -0,0 +1,7 @@
+import os
+
+########VARIABLES######
+SPOTIFY_CLIENT_ID = os.environ.get("SPOTIFY_CLIENT_ID")
+SPOTIFY_SECRET = os.environ.get("SPOTIFY_SECRET")
+SPOTIFY_USERNAME = os.environ.get("SPOTIFY_USERNAME")
+REDIRECT_URI = os.environ.get("REDIRECT_URI")
