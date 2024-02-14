80b955c4e786b85479c3a3189f61da7fd75226ac
Atsuto Tatsumi <atsutout@gmail.com>
2024-02-14 18:10:00 +0900

    Added tailor dataframe function

--- generate_playlist.py
+++ generate_playlist.py
@@ -1,8 +1,32 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-from Music_Selector_Project.params import *
+from params import *
+from preprocess_df import kaggle_preprocess
+from neuro_model import create_model
 
-def generate_playlist(df,emotion:str):
+def tailor_df(emotion:str):
+    '''This function takes emotion input from facial recognition
+    and outputs a dataframe tailored for that emotion'''
+    df = kaggle_preprocess(model=create_model()[0],scaler=create_model()[1])
+    if emotion == 'anger' or 'disgust' or 'fear':
+        mood_df = df.sort_values('mood_Calm',ascending=False).head(200)
+        mood_df = mood_df.sort_values('mood_Energetic',ascending=True).head(100).sample(10)
+
+    elif emotion == 'enthusiasm':
+        mood_df = df.sort_values('mood_Energetic',ascending=False).head(200)
+        mood_df = mood_df.sort_values('mood_Calm',ascending=True).head(100).sample(10)
+
+    elif emotion == 'happiness' or 'neutral':
+        mood_df = df.sort_values('mood_Happy',ascending=False).head(200)
+        mood_df = mood_df.sort_values('mood_Sad',ascending=True).head(100).sample(10)
+
+    else:
+        mood_df = df.sort_values('mood_Sad',ascending=False).head(200)
+        mood_df = mood_df.sort_values('mood_Happy',ascending=True).head(100).sample(10)
+
+    return mood_df
+
+def generate_playlist(emotion:str):
     '''This function will access Spotify API and add playlist to your account.
     The songs will be chosen randomly from the provided df.'''
 
@@ -20,16 +44,15 @@ def generate_playlist(df,emotion:str):
 
     user_id = sp.current_user()["id"]
 
-    new_playlist = sp.user_playlist_create(user=user_id, name=f"{emotion.upper()}_Playlist", public=True,
+    new_playlist = sp.user_playlist_create(user=user_id, name=f"{emotion.capitalize()}_Playlist", public=True,
                                       description=None)
     new_playlist_id = new_playlist["id"]
 
     # Select some music from df.
-
-    title_list_sample = list(df.sample(20)['name'])
+    title_list_sample = list(tailor_df(emotion=emotion).sample(10)['name'])
 
     uri_list = []
-    for value in range(20):
+    for value in range(10):
         spotify_result = sp.search(q=f"track:{title_list_sample[value]}",type="track", market="US")
         try:
             result_uri = spotify_result["tracks"]["items"][0]["uri"]
@@ -39,3 +62,5 @@ def generate_playlist(df,emotion:str):
             uri_list.append(result_uri)
 
     sp.user_playlist_add_tracks(user=user_id, playlist_id=new_playlist_id, tracks=uri_list)
+
+generate_playlist(emotion='Sad')
