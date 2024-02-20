f7d6c32faa8775d4159a717631efec78a6caa6d1
krystia lewis <lewiskrystia001@gmail.com>
2024-02-20 23:38:48 +0900

    added .py files for spotify interactions and team page to be used fr multi-page app

---
+++ interface/about_us.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("Meet the Team!")
+st.write("Members profile will be shown in this page with corresponding pictures on the left side")
---
+++ interface/regarding_spotify_interact.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def regarding_spotify_interact():
+    st.title("How to add the playlist to your Spotify library")
+    st.write("Add more information with regards to Spotify interaction of the App")
