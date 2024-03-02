50ea366ef779347eeff0347bf8c5823d2f57c0fe
Atsuto Tatsumi <atsutout@gmail.com>
2024-03-02 22:51:34 +0900

    Slightly changed the code

--- app.py
+++ app.py
@@ -37,7 +37,8 @@ from interface.alternative_input_preproc import is_image, image_to_video, save_u
 # temp_file = tempfile.NamedTemporaryFile()
 # downloads_path = temp_file.name
 
-OUTPUT_VIDEO_PATH = os.environ.get("VIDEO_PATH")
+#OUTPUT_VIDEO_PATH = os.environ.get("VIDEO_PATH")
+OUTPUT_VIDEO_PATH = st.secret['VIDEO_PATH']
 duration = 10
 
 # lock = threading.Lock()
