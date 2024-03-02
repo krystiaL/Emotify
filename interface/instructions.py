989eb2bcbaf0c2befedaaf4e866dc69e94eda5b2
krystia lewis <lewiskrystia001@gmail.com>
2024-03-03 08:57:10 +0900

    finished how to use info

--- interface/instructions.py
+++ interface/instructions.py
@@ -1,37 +1,53 @@
 import streamlit as st
 
-instructions = [
-                    {1:"interface/images/upload_icon.png", "step_1": "Upload a photo or video showing your face."}
-
-                    ]
-
 def instructions_page():
-
-
     col_ex1, col_ex2 = st.columns([0.5, 2])
     #step 1
     col_ex1.write(" ")
     col_ex1.image("interface/images/upload_icon.png")
-    col_ex2.write(" ")
-    col_ex2.write("Upload a photo or video showing your face.")
+    col_ex2.image("interface/images/camera_icon.png")
+    st.markdown(f"""
+                    <h1 style="font-size: 20px; text-align: center; font-family: Trebuchet MS">
+                    Choose whether to take a photo of your face using the camera or upload your own image showing your face.
+                    </h1>
+                    """, unsafe_allow_html=True)
     #step 2
-    col_ex1.subheader(" ")
-    col_ex1.image("interface/images/click_submit_icon.png")
-    col_ex2.write("Click the submit button to start the emotion extraction process.")
+    st.subheader(" ")
+    st.image("interface/images/click_generate.png")
+    st.markdown(f"""
+                    <h1 style="font-size: 20px; text-align: center; font-family: Trebuchet MS">
+                    Click the generate button to start the emotion extraction process and playlist generation.
+                    </h1>
+                    """, unsafe_allow_html=True)
+
     #step 3
-    col_ex1.title(" ")
-    col_ex1.subheader(" ")
-    col_ex1.image("interface/images/emoji_icons.png")
-    col_ex2.write("Please give the application some time to identify the emotion from the image or video file.")
+    st.title(" ")
+    st.subheader(" ")
+    st.image("interface/images/emoji_icons.png")
+    st.markdown(f"""
+                    <h1 style="font-size: 20px; text-align: center; font-family: Trebuchet MS">
+                    Please give the model some time to identify the emotion from the image.
+                    </h1>
+                    """, unsafe_allow_html=True)
+
     #step 4
-    col_ex1.title(" ")
-    col_ex1.title(" ")
-    col_ex1.subheader(" ")
-    col_ex1.image("interface/images/processing_icon.png")
-    col_ex2.write("After emotion recognition, the application will start generating the playlist based on the extracted emotion from the image/video.")
+    st.title(" ")
+    st.title(" ")
+    st.subheader(" ")
+    st.image("interface/images/processing_icon.png")
+    st.markdown(f"""
+                    <h1 style="font-size: 20px; text-align: center; font-family: Trebuchet MS">
+                    The model will start generating the playlist based on the extracted emotion from the image.
+                    </h1>
+                    """, unsafe_allow_html=True)
+
     #step 5
-    col_ex1.title(" ")
-    col_ex1.title(" ")
-    col_ex1.subheader(" ")
-    col_ex1.image("interface/images/musical_notes_icon.png")
-    col_ex2.write("Play the generated playlist from the website and/or save it to your Spotify account library.")
+    st.title(" ")
+    st.title(" ")
+    st.subheader(" ")
+    st.image("interface/images/musical_notes_icon.png")
+    st.markdown(f"""
+                    <h1 style="font-size: 20px; text-align: center; font-family: Trebuchet MS">
+                    Play songs from the generated playlist directly from the Spotify embedding or click "..." to get redirected to Spotify.
+                    </h1>
+                    """, unsafe_allow_html=True)
