8eb958d4791303cfad3ebbf627ac347c3901d3eb
krystia lewis <lewiskrystia001@gmail.com>
2024-03-03 09:17:23 +0900

    improved image alignment if icons for instructions.py

--- interface/instructions.py
+++ interface/instructions.py
@@ -1,19 +1,22 @@
 import streamlit as st
 
 def instructions_page():
-    col_ex1, col_ex2 = st.columns([0.5, 2])
     #step 1
-    col_ex1.write(" ")
-    col_ex1.image("interface/images/upload_icon.png")
-    col_ex2.image("interface/images/camera_icon.png")
+    step1_img1, step1_blank, step1_img2 = st.columns([2, 0.7, 2])
+
+    st.write(" ")
+    step1_img1.image("interface/images/upload_icon.png")
+    step1_img2.image("interface/images/camera_icon.png")
     st.markdown(f"""
                     <h1 style="font-size: 20px; text-align: center; font-family: Trebuchet MS">
                     Choose whether to take a photo of your face using the camera or upload your own image showing your face.
                     </h1>
                     """, unsafe_allow_html=True)
     #step 2
+    step2_blank1, step2_img, step2_blank2 = st.columns([1, 2, 1])
+
     st.subheader(" ")
-    st.image("interface/images/click_generate.png")
+    step2_img.image("interface/images/click_generate.png")
     st.markdown(f"""
                     <h1 style="font-size: 20px; text-align: center; font-family: Trebuchet MS">
                     Click the generate button to start the emotion extraction process and playlist generation.
@@ -21,9 +24,11 @@ def instructions_page():
                     """, unsafe_allow_html=True)
 
     #step 3
+    step3_blank1, step3_img, step3_blank2 = st.columns([1, 2, 1])
+
     st.title(" ")
     st.subheader(" ")
-    st.image("interface/images/emoji_icons.png")
+    step3_img.image("interface/images/emoji_icons.png")
     st.markdown(f"""
                     <h1 style="font-size: 20px; text-align: center; font-family: Trebuchet MS">
                     Please give the model some time to identify the emotion from the image.
@@ -31,10 +36,12 @@ def instructions_page():
                     """, unsafe_allow_html=True)
 
     #step 4
+    step4_blank1, step4_img, step4_blank2 = st.columns([1, 2, 1])
+
     st.title(" ")
     st.title(" ")
     st.subheader(" ")
-    st.image("interface/images/processing_icon.png")
+    step4_img.image("interface/images/processing_icon.png")
     st.markdown(f"""
                     <h1 style="font-size: 20px; text-align: center; font-family: Trebuchet MS">
                     The model will start generating the playlist based on the extracted emotion from the image.
@@ -42,10 +49,12 @@ def instructions_page():
                     """, unsafe_allow_html=True)
 
     #step 5
+    step5_blank1, step5_img, step5_blank2 = st.columns([1, 2, 1])
+
     st.title(" ")
     st.title(" ")
     st.subheader(" ")
-    st.image("interface/images/musical_notes_icon.png")
+    step5_img.image("interface/images/musical_notes_icon.png")
     st.markdown(f"""
                     <h1 style="font-size: 20px; text-align: center; font-family: Trebuchet MS">
                     Play songs from the generated playlist directly from the Spotify embedding or click "..." to get redirected to Spotify.
